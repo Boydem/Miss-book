f182078745c4f1c60c623b152e8994a3d35c76f7
Boydem <noyemdahan@gmail.com>
2022-12-25 22:05:34 +0200

    styled details

--- cmps/book-details.jsx
+++ cmps/book-details.jsx
@@ -1,15 +1,38 @@
 const { useState, useEffect } = React
 
 export function BookDetails({ book }) {
+  console.log("book:", book)
   return (
-    <section className="book-details flex flex-column justify-center">
+    <section className="book-details ">
       <img className="book-img" src={book.thumbnail} alt={book.title} />
-      <h1 className="book-d-title">{book.title}</h1>
-      <p className="book-d-desc muted">{book.description}</p>
-      <h5 className="book-d-price">
-        {book.listPrice.amount}
-        {book.listPrice.currencyCode}
-      </h5>
+      <div className="txt-wrapper">
+        <h1 className="book-d-title">{book.title}</h1>
+        <h2 className="book-d-subtitle">{book.subtitle}</h2>
+        <hr />
+        <div className="container flex">
+          <h4>
+            Language: <span>{book.language}</span>
+          </h4>{" "}
+          |
+          <h4>
+            Categories: <span>{book.categories.join(", ")}</span>
+          </h4>{" "}
+          |
+          <h4>
+            Pages: <span>{book.pageCount}</span>
+          </h4>{" "}
+          |
+          <h4>
+            Published: <span>{book.publishedDate}</span>
+          </h4>
+        </div>
+        <p className="book-d-desc muted">{book.description}</p>
+
+        <h3 className="book-d-price">
+          {book.listPrice.amount}
+          {book.listPrice.currencyCode}
+        </h3>
+      </div>
     </section>
   )
 }
